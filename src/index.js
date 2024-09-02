b18640bbc58ccb10d0bd806feabefc3421f4754c
Eknoor Singh <eknoorsingh1214@gmail.com>
2024-09-02 14:36:20 +0530

    another commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +16,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+=======
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import './App.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+>>>>>>> a218746 (another commit)
